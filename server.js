70380110d10e8f4944a9fe71e59c54e0fdbe8b91
coderanishka <62501236+coderanishka@users.noreply.github.com>
2022-09-15 18:07:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-        user: 'anichia145@gmail.com',
-        pass: 'qlhnhzhspegmrexo',
+        user: '',
+        pass: '',
     },
     secure: true,
 });
@@ -44,7 +44,7 @@ app.post("/send-mail", (req, res) => {
     const to = req.body.to;
     const url = req.body.url;
     const mailData = {
-        from: "anichia145@gmail.com",
+        from: "",
         to: to,
         subject: "Join the video chat with me!",
         html: `<p>Hey there,</p><p>Come and join me for a video chat here - ${url}</p>`
